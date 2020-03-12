057463a7f0443b962fa4587af7197bcf99369596
Tiago Moraes <tiagotgob@hotmail.com>
2020-03-11 23:28:20 -0300

    Tests to change activity if password is correct

---
+++ app/src/main/java/br/com/tgob/testeandroid/AccountDetails.java
@@ -0,0 +1,14 @@
+package br.com.tgob.testeandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_details);
+    }
+}
--- app/src/main/java/br/com/tgob/testeandroid/MainActivity.java
+++ app/src/main/java/br/com/tgob/testeandroid/MainActivity.java
@@ -2,7 +2,15 @@ package br.com.tgob.testeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText password = findViewById(R.id.edtPassword);
+        final EditText user = findViewById(R.id.edtUser);
+        Button login = findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!isValidPassword(password.getText().toString().trim()) && password.getText().toString().length() < 8 ) {
+                    Toast.makeText(MainActivity.this, "Password Invalid", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Password Valid", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, AccountDetails.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    public boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
     }
 }
